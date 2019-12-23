c2ee576e736325b8d32985e88d4b8957010ee6d2
Shiburaj Pappu <powerupneo@gmail.com>
2019-12-23 12:04:58 +0530

    Template and Exp1

---
+++ exp1/exp1.c
@@ -0,0 +1,31 @@
+/*
+    Aim: To print a string on output screen
+    Author: Prof. Shiburaj P.
+*/
+
+#include <stdio.h>
+int main ()
+{
+    printf("To,\n");
+    printf("The Principal,\n");
+    printf("Rizvi College of Engineering,\n");
+    printf("Bandra (W).\n\n\n");
+
+    printf("\tSubject : Requesting some books for the library,\n\n\n");
+
+    printf("Dear Sir/Madam,\n");
+    printf("\tMyself shiburaj would like to request purchase the following books for the library.\n\n");
+    printf("_________________________________________________________________________________________\n");
+    printf("|Title\t\t\t\t\t\t|Author\t\t\t|Publication\t|\n");
+    printf("|_______________________________________________|_______________________|_______________|\n");
+    printf("|C Programming \t\t\t\t\t|Balguruswami\t\t|McGraw Hill\t|\n");
+    printf("|_______________________________________________|_______________________|_______________|\n");
+    printf("|Python Programming \t\t\t\t|Simon Says\t\t|Pearsons\t|\n");
+    printf("|_______________________________________________|_______________________|_______________|\n\n\n");
+
+    printf("Thanking you in anticipation.\n");
+    printf("Regards,\n");
+    printf("Shiburaj Pappu.");
+
+    return 0;
+}
---
+++ template/template.c
@@ -0,0 +1,14 @@
+/*
+    Aim: ...
+    Author: ...
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+    // Your code starts here
+
+
+    return 0;
+}
