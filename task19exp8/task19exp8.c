c9a1ed0663a8d56e94da380c63cef7e8f7ec6784
Shiburaj Pappu <powerupneo@gmail.com>
2021-06-22 22:04:19 +0530

    BMI code corrected & All patterns code contributed by Asjad

--- task19exp8/task19exp8.c
+++ task19exp8/task19exp8.c
@@ -17,17 +17,23 @@ int main ()
     printf("Enter your Weight (in Kgs) :");
     scanf("%f",&weight);
 
-    bmi = weight/height;
-    printf("Your BMI is %f\n",bmi);
+    bmi = weight/(height*height);
+    printf("Your BMI is %.1f\n",bmi);
 
-    if(bmi > 30){
-        printf("Your are Obese");
+    if(bmi > 40){
+        printf("Your are Obese Class III");
+    }else if(bmi > 35){
+        printf("You are Obese Class II");
+    }else if(bmi > 30){
+        printf("You are Obese Class I");
     }else if(bmi > 25){
         printf("You are Overweight");
     }else if(bmi > 18.5){
-        printf("You are Normal in Weight");
+        printf("You are Normal");
+    }else if(bmi > 17){
+        printf("You are Mild Thinness");
     }else{
-        printf("You are Underweight");
+        printf("You are Severe Thinness");
     }
 
     return 0;
